59dcf413a21bf129ee97c285a25f7d25b6989b76
zhaojiachen <zhaojiachen@seclover.com>
2018-10-30 17:13:43 +0800

    邀请函 第一个较为完整的页面

---
+++ 1.invite/enroll.js
@@ -0,0 +1,10 @@
+
+var enroll = document.getElementById('enroll')
+
+enroll.onclick = function(e) {
+   e.preventDefault();
+   enroll.innerHTML = "报名成功";
+   enroll.style.background = "#27cb8b";
+
+
+}
